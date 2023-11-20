44d7b5f033b7c99db994e9d5383692ef846d9a8b
jacobthacker1 <123317273+jacobthacker1@users.noreply.github.com>
2023-11-20 10:41:09 -0600

    Working Integer Overflow
    Option 0 has been fully tested

--- main.c
+++ main.c
@@ -1,54 +1,48 @@
 #include <stdio.h>
 #include <limits.h>
 
-void vulnerableFunctionA(unsigned int a, unsigned int b) {
-    unsigned int result = 0;
-    int overflowFlag = 0;  // This flag is intended to be modified by overflow
-
-    // Storing original values for later comparison
-    unsigned int original_a = a;
-    unsigned int original_b = b;
-
-    // Adding the integers with the potential for overflow
-    result = a + b;
-
-    // Checking if overflowFlag has been altered due to overflow
-    if (overflowFlag != 0) {
-        printf("Overflow successfully executed! Variables were altered.\n");
+void vulnerableFunctionA(char a) {
+    char overflow[] = "??????????????????????????????????????????????????????????????????????????????";
+    char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    printf("%c\n", letters[a - 1]);
+    if(a < 0 || a > 26){
+        printf("Integer Overflow Detected, a ? means you have printed from our super secret overflow array.  NOOOOOOO! D': \n");
     }
-
-    // Showing the result of the addition
-    printf("Result of %u + %u is %u\n", original_a, original_b, result);
 }
 
-int main() {
-    int option;
-    unsigned int a, b;
+int main(int argc, char **argv) {
+    char *fgets_ret;
+    char input_buffer[300] = {0};
+    char c;
 
-    printf("Enter an option followed by any necessary arguments: ");
-    
-    if (scanf("%d", &option) != 1) {
-        fprintf(stderr, "Error reading input.\n");
-        return 1;
+    printf("[?] Input: ");
+    fgets_ret = fgets(input_buffer, sizeof(input_buffer), stdin);
+
+    if (fgets_ret != NULL) {
+        c = input_buffer[0];
+        switch(c) {
+            case '0':
+                printf("\n[*] %c: calling vulnerableFunctionA()\n", c);
+                vulnerableFunctionA(input_buffer[2] - '0');
+                break;
+            case '1':
+                // Option 1 code here
+                break;
+            case '2':
+                // Option 2 code here
+                break;
+            case '3':
+                // Option 3 code here
+                break;
+            default:
+                printf("\n[!] Selection %c not implemented\n", c);
+                break;
+        }
+    } else {
+        printf("\n[!] Error reading input\n");
     }
 
-    switch (option) {
-        case 1:
-            if (scanf("%u %u", &a, &b) != 2) {
-                fprintf(stderr, "Error reading integers for option 1.\n");
-                return 1;
-            }
-            vulnerableFunctionA(a, b);
-            break;
-        case 2:
-            // Option 2 code here
-            break;
-        case 3:
-            // Option 3 code here
-            break;
-        default:
-            printf("Invalid option\n");
-    }
+    printf("[*] Done!\n");
 
     return 0;
 }
